be52a1571d27c812d3bdc2abbda2d6eadcf556d2
asri6725 <abhinandan.srinivas@gmail.com>
2022-12-24 15:57:10 +1100

    lol we can download csv statements 😭

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import {ExtractText} from './pdf.js';
 
 function App() {
   return (
     <div className="App">
-    <input type="file" id="file-id" name="file_name" onChange={() => ExtractText()} />
-    <div id="output"></div>
-    <div id="json"></div>
+    Hello World!
   </div>
   );
 }
