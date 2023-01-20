0048293ba99d932184afbee66492f3e96f10fb4e
abhi-TU <asrinivas@transurban.com>
2023-01-20 17:38:16 +1100

    Display in tabular format - issue with tables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
-import CsvUploader from './CsvUploader';
-
+import CSVReader from './CsvReader';
+import React, { useState } from 'react';
+import TransactionTable from './TransactionTable';
 function App() {
+  const [fileData, setFileData] = useState([]);
+  const pull_data = (data) => {
+    console.log(data)
+
+    setFileData(data.data)
+  }
   return (
     <div className="App">
-    <CsvUploader />
+      <div className='Title'>
+        <h1> Finances </h1>
+      </div>
+      <div className='Import data'> 
+        <CSVReader pull_data={pull_data}/> 
+      </div>
+      <div className='Output'>
+        <p>{fileData}</p>
+        <div className='TransactionTable'>
+          <TransactionTable fileData = {fileData}/>
+        </div>
+      </div>
   </div>
   );
 }
